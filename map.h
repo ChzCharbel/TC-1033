c77e028d381aa435059e3b08338bef20a7a767be
Charbel Chavez <121769083+ChzCharbel@users.noreply.github.com>
2023-11-16 00:29:35 -0600

    Update map.h

--- map.h
+++ map.h
@@ -7,18 +7,21 @@ private:
   string name;
   string location;
   float size;
+  string site;
 
 public:
-  Map():name(""), location(""), size(0.0){};
-  Map(string nm, string lctn, float sz): name(nm), location(lctn), size(sz){};
+  Map():name(""), location(""), size(0.0), site(""){};
+  Map(string nm, string lctn, float sz, string st): name(nm), location(lctn), size(sz), site(st){};
 
   string get_name();
   string get_location();
   float get_size();
+  string get_site();
 
   void set_name(string );
   void set_location(string );
   void set_size(float );
+  void set_site(string );
 
 };
 
@@ -34,6 +37,10 @@ float Map::get_size(){
   return size;
 }
 
+string Map::get_site(){
+  return site;
+}
+
 void Map::set_name(string nm){
   name = nm;
 }
@@ -44,4 +51,8 @@ void Map::set_location(string lctn){
 
 void Map::set_size(float sz){
   size = sz;
+}
+
+void Map::set_site(string st){
+  site = st;
 }
