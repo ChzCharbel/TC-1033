36d4ca32fb421de786d61c33bb31cff318d4ac53
Charbel Chavez <121769083+ChzCharbel@users.noreply.github.com>
2023-11-16 00:28:55 -0600

    Update gun.h

--- gun.h
+++ gun.h
@@ -4,26 +4,33 @@ using namespace std;
 
 class Gun{
 private:
-  string skin;
+  string name;
+  string type;
   float fire_rate;
   int ammo;
 
 public:
-  Gun():skin(""), fire_rate(0.0), ammo(0){};
-  Gun(string skn, float fr, int amm): skin(skn), fire_rate(fr), ammo(amm){};
+  Gun():name(""), type(""), fire_rate(0.0), ammo(0){};
+  Gun(string nm, string typ, float fr, int amm): name(nm), type(typ),fire_rate(fr), ammo(amm){};
 
-  string get_skin();
+  string get_name();
+  string get_type();
   float get_fire_rate();
   int get_ammo();
 
-  void set_skin(string );
+  void set_name(string );
+  void set_type(string );
   void set_fire_rate(float );
   void set_ammo(int );
 
 };
 
-string Gun::get_skin(){
-  return skin;
+string Gun::get_name(){
+  return name;
+}
+
+string Gun::get_type(){
+  return type;
 }
 
 float Gun::get_fire_rate(){
@@ -34,8 +41,12 @@ int Gun::get_ammo(){
   return ammo;
 }
 
-void Gun::set_skin(string skn){
-  skin = skn;
+void Gun::set_name(string nm){
+  name = nm;
+}
+
+void Gun::set_type(string typ){
+  type = typ;
 }
 
 void Gun::set_fire_rate(float fr){
